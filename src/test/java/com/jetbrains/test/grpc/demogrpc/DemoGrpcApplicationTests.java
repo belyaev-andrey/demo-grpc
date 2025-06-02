1b998d90de8cf655da526a0656526c8ec5822590
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-06-02 15:55:19 +0200

    Initial version

---
+++ src/test/java/com/jetbrains/test/grpc/demogrpc/DemoGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.test.grpc.demogrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
